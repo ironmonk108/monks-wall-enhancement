26b5a9921561d7e9ab3c6c104f685fa4b38a7cf2
Ian Bennett <34078802+ian612@users.noreply.github.com>
2025-09-12 00:43:47 -0400

    Fix broken context menu for compendium scenes

--- monks-wall-enhancement.js
+++ monks-wall-enhancement.js
@@ -1447,7 +1447,7 @@ Hooks.on("getSceneContextOptions", (html, menu) => {
     menu.push({
         name: "Close All Doors",
         icon: '<i class="fas fa-door-open"></i>',
-        condition: li => game.user.isGM && game.scenes.get(li.dataset.sceneId || li.dataset.entryId).active,
+        condition: li => game.user.isGM && game.scenes.get(li.dataset.sceneId || li.dataset.entryId)?.active,
         callback: li => {
             let scene = game.scenes.get(li.dataset.sceneId || li.dataset.entryId);
             if (scene)
@@ -1513,5 +1513,6 @@ Hooks.on('renderWallConfig', async (app, html, options) => {
                     },
                     default: "save"
                 }).render(true);
+
             }));
 });
